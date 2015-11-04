67b2ea279060bb7cf61e7dd201f27eddca872cfb
magnus udbjorg <magnus@udbjorg.net>
2015-11-04 21:20:07 +0100

    adding mq

--- app/controllers/Services.scala
+++ app/controllers/Services.scala
@@ -1,70 +1,45 @@
 package controllers
 
-import com.rabbitmq.client.AMQP.BasicProperties
-import com.rabbitmq.client.QueueingConsumer.Delivery
-import com.rabbitmq.client.{ConnectionFactory, QueueingConsumer}
+import com.rabbitmq.client.ConnectionFactory
+import play.api.Play
+import play.api.libs.json.Json
 import play.api.mvc.{Action, Controller}
 
-import scala.concurrent.Future
-
 class Services extends Controller {
-
-
   val factory = new ConnectionFactory()
-  factory.setHost("localhost")
-  val connection = factory.newConnection()
-  val rpcChannel = connection.createChannel()
-  val requestQueueName = "rpc_link_queue"
-  val replyQueueName = rpcChannel.queueDeclare().getQueue()
-  val consumer = new QueueingConsumer(rpcChannel)
-  rpcChannel.basicConsume(replyQueueName, true, consumer)
-
-
-  def toBytes(x: String) = x.getBytes("UTF-8")
-  def fromBytes(x: Array[Byte]) = new String(x, "UTF-8")
-  def whileLoop(cond : =>Boolean)(block : =>Unit) : Unit =
-    if(cond) {
-      block
-      whileLoop(cond)(block)
-    }
+  val host = Play.current.configuration.getString("rabbitmq.host").get
+  factory.setHost(host)
+  val connection = factory.newConnection();
+  val sendingChannel = connection.createChannel();
+  val exchange = "amq.fanout"
 
   def linkAdd(url: Option[String], title: Option[String], summary: Option[String]) = Action { request =>
+
+
+
     for {
       urlString <- url
       titleString <- title
       summaryString <- summary
       userId <- request.session.get("userId")
     } yield {
+      val jsonObject = Json.toJson(
+           Map(
+                "url" -> Json.toJson(urlString),
+                "title" -> Json.toJson(titleString),
+                "summary" -> Json.toJson(summaryString)
+              )
+
+        )
+      val jsonString: String = Json.stringify(jsonObject)
+
+      sendingChannel.basicPublish(exchange, "links.add", null, jsonString.getBytes())
 
     }
     Ok("Send for creation")
   }
 
-  def linkInfo(url_option: Option[String]) = Action.async {
-
-    val corrId = java.util.UUID.randomUUID().toString()
-
-    val props = new BasicProperties
-    .Builder()
-      .correlationId(corrId)
-      .replyTo(replyQueueName)
-      .build()
-
-      val url:String = url_option map { _.toLowerCase() } getOrElse ""
-
-      if(url.isEmpty){
-        Future.successful(BadRequest("No url supplied"))
-      }
-      rpcChannel.basicPublish("", requestQueueName, props, url.getBytes())
-     val response:String  = getRPCCallback(corrId, consumer.nextDelivery())
-
-
-    Future.successful(Ok(response))
+  def linkInfo(url_option: Option[String]) = Action {
+    Ok("Send for creation")
   }
-  def getRPCCallback(id: String, delivery:Delivery): String = delivery.getProperties().getCorrelationId().equals(id) match{
-    case true => return fromBytes(delivery.getBody())
-    case false => getRPCCallback(id, consumer.nextDelivery())
-  }
-
-
 }
